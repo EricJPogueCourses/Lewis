9dd50df916ae36a99a0862be828ce9fb1b6ca139
EricJPogue <epogue@epogue.com>
2020-05-22 13:39:56 -0500

    Update file.

--- src/Main.js
+++ src/Main.js
@@ -78,6 +78,7 @@ class Main extends Component {
 		// Example 4b: https://www.lewis.education/?course=25000
 		// Example 5b: https://www.lewis.education/
 		// Example 6b: https://www.lewis.education/ 
+		// Example 7b: https://www.lewis.education/?course=25000
 
 		const defaultCourseNumber = 25000 /* default to OOP (cpsc-25000) */ ;
 		const queryString = window.location.search;
