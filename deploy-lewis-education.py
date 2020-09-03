073b5031fb33f0caae1bda5b94712faa51bf611b
EricJPogue <epogue@epogue.com>
2020-09-03 13:11:53 -0500

    Update React client.

--- deploy-lewis-education.py
+++ deploy-lewis-education.py
@@ -1,5 +1,6 @@
 import os
 os.system("cd /Users/epogue/Repositories/lewis-education")
+os.system("git status")
 os.system("git add .")
 os.system("git commit -a -m \"Update React client.\"")
 os.system("git push")
