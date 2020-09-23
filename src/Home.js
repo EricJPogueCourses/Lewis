8e45e6712dbfe3ddd6c5f666c98250867dae151c
EricJPogue <epogue@epogue.com>
2020-09-23 09:36:26 -0500

    Update file.

--- src/Home.js
+++ src/Home.js
@@ -9,16 +9,16 @@ export class Home extends React.Component {
 <h5>Welcome to Fall 2020 at Lewis.education!</h5>
 <br />
 <h5>Introduction to Computer Science:</h5> 
-<h6>MWF 10am CT (002)<a href= 'http://localhost:3000/?cpsc=20000-fall-2020-002/#/sprint-1'>[link]</a></h6>
-<h6>MWF 11am CT (003)<a href= 'http://localhost:3000/?cpsc=20000-fall-2020-003/#/sprint-1'>[link]</a></h6>
+<h6>MWF 10am CT (002)<a href= 'https://www.lewis.education/?cpsc=20000-fall-2020-002/#/sprint-1'>[link]</a></h6>
+<h6>MWF 11am CT (003)<a href= 'https://www.lewis.education/?cpsc=20000-fall-2020-003/#/sprint-1'>[link]</a></h6>
 <br />
 <h5>Web &amp; Distributed Programming:</h5>
-<h6>MWF 3pm CT (001)<a href= 'http://localhost:3000/?cpsc=24700-fall-2020-002/#/sprint-1'>[link]</a></h6>
+<h6>MWF 3pm CT (001)<a href= 'https://www.lewis.education/?cpsc=24700-fall-2020-002/#/sprint-1'>[link]</a></h6>
 <br />
 <h5>Software Engineering:</h5>
-<h6>MWF 12 noon CT (001)<a href= 'http://localhost:3000/?cpsc=44000-fall-2020-001/#/sprint-1'>[link]</a></h6>
+<h6>MWF 12 noon CT (001)<a href= 'https://www.lewis.education/?cpsc=44000-fall-2020-001/#/sprint-1'>[link]</a></h6>
 <br /><br />
-<p>If you are looking for more information on usage and testing of the Lewis.education application, please see the following<a href= 'http://localhost:3000/#/test/me'>[link]</a>.</p>
+<p>If you are looking for more information on usage and testing of the Lewis.education application, please see the following<a href= 'https://www.lewis.education/#/test/me'>[link]</a>.</p>
 </div>
 
 		)
