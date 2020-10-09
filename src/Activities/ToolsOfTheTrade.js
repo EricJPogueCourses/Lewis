4cd6820ea1cbc6c8aaeb731f3fe7d16ce826de60
EricJPogue <epogue@epogue.com>
2020-10-09 14:38:26 -0500

    Update Tools of The Trade.

--- src/Activities/ToolsOfTheTrade.js
+++ src/Activities/ToolsOfTheTrade.js
@@ -22,7 +22,7 @@ install, configure, and utilize many tools and services. By the end of this tuto
 	<li>Install FireFox<a href='https://www.mozilla.org/en-US/firefox/new/' target='_blank' rel='noopener noreferrer'>[link]</a>and Chrome<a href='https://www.google.com/chrome/' target='_blank' rel='noopener noreferrer'>[link]</a>web browsers</li> 
 	<li>Install Visual Studio Code source code editor<a href='https://code.visualstudio.com/' target='_blank' rel='noopener noreferrer'>[link]</a><NavLink to='/activity-config/mscode-and-chrome-windows10-setup'>[video]</NavLink></li>
 	<li>Install the Visual Studio Code and the Python 3 programming environment<a href='https://code.visualstudio.com/docs/python/python-tutorial' target='_blank' rel='noopener noreferrer'>[link]</a></li>
-	<li>Configure Office 365 Outlook and Teams which are available to you free through Lewis University<a href='https://www.office.com/' target='_blank' rel='noopener noreferrer'>[link]</a></li>
+	<li>Configure Microsoft Office 365 Outlook and Teams which are available to you free through Lewis University<a href='https://www.office.com/' target='_blank' rel='noopener noreferrer'>[link]</a></li>
 	<li>Sign up for OReilly Safari Books Online service which is available free through Lewis University<a href='https://www.oreilly.com/library/view/temporary-access/' target='_blank' rel='noopener noreferrer'>[link]</a></li>
 	<li>Utilize File Explorer (Windows) or Finder (MacOS) to manage folders, files, file 
 		extensions, zip files, and hidden folders/files</li>
@@ -44,23 +44,25 @@ install, configure, and utilize many tools and services. By the end of this tuto
 			<li>Review the “Git Handbook”<a href='https://guides.github.com/introduction/git-handbook/' target='_blank' rel='noopener noreferrer'>[link]</a>
 			and the “GitHub Git Cheat Sheet”<a href='https://github.github.com/training-kit/downloads/github-git-cheat-sheet.pdf' target='_blank' rel='noopener noreferrer'>[link]</a></li> 
 		</ul></li>
-	<li>Configure MS Office Teams from above or Discord for your Scrum team<a href='https://discord.com/' target='_blank' rel='noopener noreferrer'>[discord]</a></li>
+	<li>Configure Teams or Discord to allow your scrum team effectively communicate<a href='https://discord.com/' target='_blank' rel='noopener noreferrer'>[discord]</a></li>
 	<li>Install Node.js and NPM<a href='https://nodejs.org/en/' target='_blank' rel='noopener noreferrer'>[link]</a></li>
 	<li>Optionally install and verify Java Open SDK<a href='https://docs.microsoft.com/en-us/java/azure/jdk/?view=azure-java-stable' target='_blank' rel='noopener noreferrer'>[link]</a>
 		<NavLink to='/activity-config/java-jdk-windows10-setup'>[video]</NavLink></li>
 </ol>
 
 <br />
-Chocolatey (preview): You may optionally install and utilize the Chocolatey (Windows) package manager to install the 
-above applications:
+<h6>Package Manger Alternatives:</h6>
+<p>An alternative method of installing, updating, and deleting software applications is to utilize a package manager. 
+Either Chocolatey (Windows) and Homebrew (MacOS) can be utilized as an alternative method to install the above applications.</p>
+<p>Chocolatey (Windows):</p>
 <ol>
 	<li>Install Chocolatey with PowerShell<NavLink to='/activity-config/chocolatey'>[video]</NavLink></li>
 	<li>Install VS Code, Python3, VS Code Python extension, MS Teams, Git, Node.js, and OpenJDK using Chocolatey<NavLink to='/activity-config/chocolatey-install'>[video]</NavLink>
 		<a href='https://lewiseducation.blob.core.windows.net/configuration/choco-install-packages.txt' target='_blank' rel='noopener noreferrer'>[script]</a>
 	</li>
 </ol>
-Homebrew (double preview): Install Homebrew and then install VS Code, Python3, VS Code Python extension, MS Teams, Git, Node.js, and OpenJDK using Homebrew
-	<a href='https://lewiseducation.blob.core.windows.net/configuration/choco-install-packages.txt' target='_blank' rel='noopener noreferrer'>[video]</a>
+<p>Homebrew (MacOS): Install Homebrew and Visual Studio Code, Python3, Visual Studio Code Python extension, MS Teams, 
+Git, Node.js, and OpenJDK<a href='https://lewiseducation.blob.core.windows.net/configuration/homebrew.mp4' target='_blank' rel='noopener noreferrer'>[video]</a></p>
 
 </div>
 
