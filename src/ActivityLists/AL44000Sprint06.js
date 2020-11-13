697b2091b36ad5fd0c0dd63b705360f105d9b4bc
EricJPogue <epogue@epogue.com>
2020-11-13 14:37:57 -0600

    Update file.

--- src/ActivityLists/AL44000Sprint06.js
+++ src/ActivityLists/AL44000Sprint06.js
@@ -53,6 +53,7 @@ export const al44000Sprint06 = () => {
 	<p>Additional references:</p>
 	<ul style={{listStyleType:'square'}}>
 		<li>Complete Programming Together with “React On Azure”<a href='https://app.knowmia.com/bBVA'target='_blank' rel='noopener noreferrer'>[link]</a></li>
+		<li>Recall “Deploying Node.js on Microsoft Azure”<a href='https://app.knowmia.com/lyyA' target='_blank' rel='noopener noreferrer'>[link]</a></li>		
 		<li>Azure serverless computing tutorial
 			<a href='https://mikepfeiffer.io/blog/azure-serverless-101'
 			target='_blank' rel='noopener noreferrer'>[link]</a></li>
