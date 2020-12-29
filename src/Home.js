7ec997d46347f7b7858e1dc5bd78fd5786c21e28
EricJPogue <epogue@epogue.com>
2020-12-28 18:35:46 -0600

    Update home screen.

--- src/Home.js
+++ src/Home.js
@@ -8,10 +8,18 @@ export class Home extends React.Component {
 <h4>Lewis.education</h4>
 <h5>Welcome to Sprint 2021 at Lewis.education!</h5>
 <br />
-<h5>Software Engineering:</h5>
-<h6>TTh 11am  noon CT (001)<a href= 'https://www.lewis.education/?cpsc=44000-spring-2021-001#/sprint-1'>[link]</a></h6>
+<h5>Introduction to Computer Science (20000-002):</h5>
+<h6>MWF 1pm CT<a href= 'https://www.lewis.education/?cpsc=20000-spring-2021-002#/sprint-1'>[link]</a></h6>
+<br />
+<h5>Object-Oriented Programming (24500-001)</h5>
+<h6>MWF 2pm CT<a href= 'https://www.lewis.education/?cpsc=24500-spring-2021-001#/sprint-1'>[link]</a></h6>
+<br />
+<h5>Software Engineering (36000-001):</h5>
+<h6>TTh 11am CT<a href= 'https://www.lewis.education/?cpsc=44000-spring-2021-001#/sprint-1'>[link]</a></h6>
+<br />
+<h5>Applied Programming Languages (44000-001):</h5>
+<h6>TTh 12:30pm CT<a href= 'https://www.lewis.education/?cpsc=36000-spring-2021-001#/sprint-1'>[link]</a></h6>
 <br /><br /><br />
-
 <h5>Depreciated Fall 2020 at Lewis.education links!</h5>
 <br />
 <h5>Introduction to Computer Science:</h5> 
