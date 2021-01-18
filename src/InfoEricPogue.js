a8822eca251098e914a0ab5d66339818563dac21
EricJPogue <epogue@epogue.com>
2021-01-18 13:54:25 -0600

    Update instructor information.

--- src/InfoEricPogue.js
+++ src/InfoEricPogue.js
@@ -11,19 +11,21 @@ export const InfoEricPogue = () => {
 <h5>Eric Pogue</h5>
 Email: <a href='mailto:webmaster@example.com'>epogue@lewisu.edu</a><br />
 Mobile Phone: <a href='tel: +15632097280'>(563)209-7280</a><br />
+Personal Discord “Lewis” Server (preview)<a href='https://discord.gg/5GEgy5nEce' target='_blank' rel='noopener noreferrer'>[link]</a><br />
 Short Bio: Professional software developer and software development manager turned college 
 instructor.<br /><br />
 
 <p>How to Contact Me?</p>
-<p>For most questions please utilize our <em>“Class Forum” Discussion Board (preferred)</em> or 
-email me directly. History suggests that posts to our Class Forum get the quickest and highest 
-quality responses. However, if your question is of personal nature, email is your best alternative. 
-In either case, you can expect a maximum response time of 24 hours weekdays.</p>
+<p>For most questions please utilize our <em>“Class Forum” Discussion Board</em>. History suggests that posts to our 
+Class Forum get the quickest and highest quality responses. However, if your question is of personal nature, email is 
+your best alternative. In either case, you can expect a maximum response time of 24 hours weekdays.</p>
 
-<p>If you need an immediate response, call my mobile phone.</p>
-<br /><br />
-<p>Need help with University Resources including the Office of Technology or the Library? Here is a good place to start
-<NavLink to='/getting-help'>[link]</NavLink></p>
+<p>For weekend questions that need quick responses the “Class Forum” is definitely your best bet. </p>
+
+<p>If you need immediate response, call my mobile phone.</p>
+<br />
+<p>Need help with University Resources including the Office of Technology, Tutoring, or the Library? Here is a good 
+place to start<NavLink to='/getting-help'>[link]</NavLink></p>
 </div>
 
 	)
--- src/Panopto.js
+++ src/Panopto.js
@@ -31,3 +31,26 @@ const IFrameComponent = (props) =>{
 const panoptoWelcomeIFrameTag = () => {
 	return "<iframe src='https://lewisu.hosted.panopto.com/Panopto/Pages/Embed.aspx?id=ea0cac63-6820-411f-b31a-acaf01489295&autoplay=true&offerviewer=false&showtitle=false&showbrand=false&start=0&interactivity=all' height='576' width='1024' style='border: 1px solid #464646;' allowfullscreen allow='autoplay'></iframe>"
 }
+
+
+
+export const programmingTogether = () => {
+	return (
+		<div>
+		<h4>{URLParameters.title()}</h4>
+		<h5>Programming Together with Hello World Plus for the Web</h5>
+		<p style={{marginTop:'20px'}}>Please take a few minutes to view your spring 2021 welcome message.</p>
+		<IFrameComponent iframe={panoptoWelcomeIFrameTag()} />
+		<br />
+		<p>Prior to our first class session, please also take a few minutes to gain a better understand of 
+		Blended Learning and Scrum by reviewing the following videos.</p>
+		<ul>
+			<li>“Blended Learning &amp; Flipped Classroom” 
+				<NavLink to='/activity/blended-learning'>[video]</NavLink></li>
+			<li>“Introduction to Scrum in 7 Minutes”
+				<NavLink to='/activity/scrum-in-7-minutes'>[video]</NavLink></li> 
+		</ul>
+		Finally, you will want to take a look at our course syllabus<a href={URLParameters.classSyllabusURL()}>[link]</a> 
+			and obtain the items identified in the “IV. Course Materials.”
+		</div> )
+}
