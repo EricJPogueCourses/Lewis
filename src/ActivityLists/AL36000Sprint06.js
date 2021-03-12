76be3c152494f5b68d86f6e8bce9a3cebe6c5065
EricJPogue <epogue@epogue.com>
2021-03-12 12:07:57 -0600

    Update files.

--- src/ActivityLists/AL36000Sprint06.js
+++ src/ActivityLists/AL36000Sprint06.js
@@ -9,7 +9,7 @@ export const al36000Sprint06 = () => {
 
 <div>
 <h4>Applied Programming Languages</h4>
-<h5>Sprint 6: React, React on Azure, Continuous Integration, and Continuous Deployment</h5>
+<h5>Sprint 6: React, React on Azure, Continuous Integration and Continuous Deployment (CI/CD)</h5>
 <p>Let’s develop some React web applications on Microsoft Azure.</p>
 
 <p>By the end of the sprint we will be able to:</p>
@@ -27,15 +27,12 @@ export const al36000Sprint06 = () => {
 	<li>{start(programmingAssignment)}</li>
 	{/* The above items should be consistent across classes. */ }
 
-	<li>Implement CI/CD </li>
-	<li>Review “Azure Website”<NavLink to='/activity/azure-website'>[link]</NavLink></li>
-
-	<li>Complete “React - The Complete Guide” chapters 1 and 2<a href='https://learning.oreilly.com/videos/react-the/9781789132229/' target='_blank' rel='noopener noreferrer'>[link]</a></li>
-	<li>Read “Learning React, 2nd Edition” chapters 1 and 2<a href='https://learning.oreilly.com/library/view/learning-react-2nd/9781492051718/' target='_blank' rel='noopener noreferrer'>[link]</a></li>
-
-	<li>Analyze “Azure Static Web Apps”<a href='aka.ms/swadocs' target='_blank' rel='noopener noreferrer'>[link]</a> focusing on React and single HTML, CSS, and Java script applications (no framework)</li>
+	<li>Focus on CI/CD by completing several React and “no framework” tutorials from “Azure Static Web Apps”<a href='https://aka.ms/swadocs' target='_blank' rel='noopener noreferrer'>[link]</a></li>
 	<li>Complete “React on Azure” programming together<NavLink to='/activity-config/react-on-azure'>[video]</NavLink></li>
+	<li>Read “Learning React, 2nd Edition” Eve Porcello and Alex Banks chapters 1 and 2<a href='https://learning.oreilly.com/library/view/learning-react-2nd/9781492051718/' target='_blank' rel='noopener noreferrer'>[link]</a></li>
 	<li><em>Make your initial Discussion post by the middle of the sprint</em></li>
+	<li>Complete “React - The Complete Guide” by Maximilian Schwarzmüller chapters 1 and 2<a href='https://learning.oreilly.com/videos/react-the/9781789132229/' target='_blank' rel='noopener noreferrer'>[link]</a></li>
+	<li>Review “Azure Website”<NavLink to='/activity/azure-website'>[link]</NavLink></li>
 
 	{/* The items below should be consistent across classes. */ }
 	<li>{reviewAndUpdate(FinalProjectProposal)}</li>
